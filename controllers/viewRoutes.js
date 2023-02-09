7fba4ab81f6d573ea038796ab50cbfaffd4a40f7
James Harrington <jamesharrington585@hotmail.com>
2023-02-08 21:32:17 -0500

    got handlebars to function

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
-const { YourCustomModel } = require('../models');
+const { Bootcamp } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
+    res.render('bootcamp', { 
+      greeting: 'hello world!'
     });
+
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
