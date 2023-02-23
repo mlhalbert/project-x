467649807219a424dc61f14f1941a99e9d18d284
James Harrington <jamesharrington585@hotmail.com>
2023-02-23 17:58:08 -0500

    tweaked css a bit, finally got a conditional to recognise when every box is clicked, moved confetti to end of game

--- public/js/script.js
+++ public/js/script.js
@@ -14,59 +14,17 @@ function selected(event) {
         document.removeEventListener('click', selected)
     } else {
     const square = event.target;
-    confetti.start();
-    setTimeout(function () { confetti.stop(); }, 1000)
+    
     square.classList.add("clicked");
-    console.log(square);
+    
+    console.log(check.length);
     console.log(check);
+    
+    }
+    if (check.length == box.length) {
+        confetti.start();
+    setTimeout(function () { confetti.stop(); }, 8000)
+        alert("YAY YOU WON OH MY GOD WOOOOOOO!!");
     }
     
 };
-
-// let tableTwo = box.classList.contains("clicked");
-// console.log(tableTwo);
-
-
-// if (table.children.length == table.querySelectorAll("clicked").length)  {
-//     console.log("------------------------worked--------------------------");
-// };
-function audit() {
-console.log(check.length);
-console.log(box.length);
-
-// if (check.length === box.length) {
-//     console.log(fuck);
-// }
-};
-// function validate() {
-//     // event.preventDefault();
-//     var thing = document.getElementsByClassName('clicked');
-//     for (var i=0; i<thing.length; i++) {
-//         if (!thing[i].classList.contains("word")) {
-//            return alert("fuck you"); 
-//         } 
-//     }
-//     console.log("yay");
-// }
-
-// console.log(['word'].every(e=>table.classList.contains(e)))
-// console.log(['word', 'clicked'].every(e=>table.classList.contains(e)))
-
-
-
-// function checkstat() {
-// if (table.length.classList.contains("clicked")){
-// 	//return true
-//   console.log('true');
-// }
-// else{
-// 	//return false
-//   console.log('false');
-// }
-// };
-
-// checkstat();
-
-// validate();
-
-audit();
